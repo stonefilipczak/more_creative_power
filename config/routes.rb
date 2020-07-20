92ad10f2e127026ed8fa7830c0d3cb5b4a8b9325
Stone Filipczak <stonejacobs@gmail.com>
2020-07-20 11:46:23 -0400

    install devise, activeadmin, bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+  devise_for :users
   resources :art_works
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "art_works#index"
 end
