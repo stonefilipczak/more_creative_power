e366181b8613ccd7e69e13f793b4ec618bea59a1
Stone Filipczak <stonejacobs@gmail.com>
2020-07-23 16:08:54 -0400

    admin images

--- Gemfile
+++ Gemfile
@@ -31,6 +31,7 @@ gem 'bootsnap', '>= 1.4.2', require: false
 gem 'bootstrap', '~> 4.3.1'
 gem 'devise'
 gem 'activeadmin'
+gem 'image_processing', '~> 1.2' # Use Active Storage variant
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/admin/art_works.rb
+++ app/admin/art_works.rb
@@ -1,3 +1,34 @@
 ActiveAdmin.register ArtWork do
-  permit_params :title, :description
+  permit_params :title, :description, :image
+  includes image_attachments: :blob
+  
+  index do
+    selectable_column
+    column :title do |art|
+      auto_link art
+    end
+    column :image do |art|
+      if art.image.attached?
+        art.image.each do |img|
+          span link_to(image_tag(img.representation(resize_to_limit: [100, 100]), alt: art.title), admin_art_work_path(art))
+        end
+      end 
+    end   
+  end
+
+  show do 
+    attributes_table do 
+      row :title
+      row :description
+      row :image do |art|
+        if art.image.attached?
+          art.image.each do |img|
+            span link_to(image_tag(img.representation(resize_to_limit: [100, 100]), alt: art.title), admin_art_work_path(art))
+          end
+        end 
+      end   
+    end  
+  end
+
+
 end
