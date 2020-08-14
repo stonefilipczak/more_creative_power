6b25e49716350420a62cbc76875c238f56e64a5b
Stone Filipczak <stonejacobs@gmail.com>
2020-08-14 16:22:34 -0400

    create photographer wizard

--- app/controllers/wizard_controller.rb
+++ app/controllers/wizard_controller.rb
@@ -1,8 +1,18 @@
 class WizardController < ApplicationController
     def welcome
         @artworks = ArtWork.where(:user => current_user)
-        if @artworks.present?
-            redirect_to art_works_path
-        end    
-    end    
+        if current_user.present? 
+            if current_user.is_photographer? 
+                photographer
+            else @artworks.present?
+                redirect_to art_works_path
+            end   
+        end
+    end
+    
+    def photographer
+        @art_works = ArtWork.all()
+        @photo_requests = PhotoRequest.all()
+        render :photographer
+    end
 end    
